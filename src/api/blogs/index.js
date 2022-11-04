297a1aa4ae87d24538782274f5f57ae8a2fe8391
Riccardo Gulin <riccardo@nucleode.com>
2022-11-04 12:59:49 +0100

    Adds operations on junction table

--- src/api/blogs/index.js
+++ src/api/blogs/index.js
@@ -1,13 +1,23 @@
 import express from "express"
+import createHttpError from "http-errors"
 import BlogsModel from "./model.js"
 import UsersModel from "../users/model.js"
-import createHttpError from "http-errors"
+import BlogsCategoriesModel from "./blogsCategoriesModel.js"
+import CategoriesModel from "../categories/model.js"
 
 const blogsRouter = express.Router()
 
 blogsRouter.post("/", async (req, res, next) => {
   try {
     const { blogsId } = await BlogsModel.create(req.body)
+    console.log(blogsId)
+    if (req.body.categories) {
+      await BlogsCategoriesModel.bulkCreate(
+        req.body.categories.map(category => {
+          return { categoryId: category, blogId: blogsId }
+        })
+      )
+    }
     res.status(201).send({ id: blogsId })
   } catch (error) {
     next(error)
@@ -17,7 +27,10 @@ blogsRouter.post("/", async (req, res, next) => {
 blogsRouter.get("/", async (req, res, next) => {
   try {
     const blogs = await BlogsModel.findAll({
-      include: { model: UsersModel, attributes: ["firstName", "lastName"] },
+      include: [
+        { model: UsersModel, attributes: ["firstName", "lastName"] },
+        { model: CategoriesModel, attributes: ["name"], through: { attributes: [] } },
+      ],
     })
     res.send(blogs)
   } catch (error) {
@@ -27,7 +40,13 @@ blogsRouter.get("/", async (req, res, next) => {
 
 blogsRouter.get("/:blogId", async (req, res, next) => {
   try {
-    const blog = await BlogsModel.findByPk(req.params.blogId, { include: { model: UsersModel } })
+    const blog = await BlogsModel.findByPk(req.params.blogId, {
+      include: [
+        { model: UsersModel, attributes: ["firstName", "lastName"] },
+        { model: CategoriesModel, attributes: ["name"], through: { attributes: [] } },
+        // to exclude the junction table record from the result we have to use through: {attributes: []}
+      ],
+    })
     if (blog) {
       res.send(blog)
     } else {
@@ -63,6 +82,18 @@ blogsRouter.delete("/:blogId", async (req, res, next) => {
     } else {
       next(createHttpError(404, `Blog with id ${req.params.blogId} not found!`))
     }
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.post("/:blogId/category", async (req, res, next) => {
+  try {
+    const { id } = await BlogsCategoriesModel.create({
+      blogId: req.params.blogId,
+      categoryId: req.body.categoryId,
+    })
+    res.status(201).send({ id })
   } catch (error) {
     next(error)
   }
